a531a5538821761b82025c5ab4f4696de89a61cc
Gabriel Fronchak Gmack <gabrielgmack@gmail.com>
2023-08-18 19:44:59 -0300

    Concluded test to login method

--- app/Services/Authservice.php
+++ app/Services/Authservice.php
@@ -2,6 +2,7 @@
 
 namespace App\Services;
 
+use App\Exceptions\UnhauthorizationException;
 use App\Models\User;
 
 class Authservice
@@ -24,10 +25,21 @@ class Authservice
 
     protected function authenticate($credentials) {
         $token = auth()->attempt($credentials);
+        if(!$token) {
+            throw new UnhauthorizationException("");
+        }
         return [
             'access_token' => $token,
             'token_type' => 'bearer'
         ];
+    }
+
+    public function login($email, $password)
+    {
+        return $this->authenticate([
+            'email' => $email,
+            'password' => $password
+        ]);
     }
 }
 
--- tests/Feature/AuthServiceTest.php
+++ tests/Feature/AuthServiceTest.php
@@ -2,13 +2,12 @@
 
 namespace Tests\Feature;
 
+use App\Exceptions\UnhauthorizationException;
 use App\Models\User;
 use App\Services\Authservice;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
-
-use function PHPUnit\Framework\assertNotEmpty;
 
 class AuthServiceTest extends TestCase
 {
@@ -34,5 +33,42 @@ class AuthServiceTest extends TestCase
         $this->assertDatabaseCount('users', 1);
         $user = User::all()->first();
         $this->assertNotEquals($user->password, 'password');
+    }
+
+    public function test_login_should_return_token_if_password_is_correct(): void
+    {
+        User::factory()->create([
+            'email' => 'user@gmail.com',
+            'password' => bcrypt('123456')
+        ]);
+
+        $result = $this->authService->login('user@gmail.com', '123456');
+
+        $this->assertArrayHasKey('access_token', $result);
+        $this->assertNotEmpty($result['access_token']);
+    }
+
+    public function test_login_should_throw_unhauthorization_exception_when_password_is_incorrect(): void
+    {
+        User::factory()->create([
+            'email' => 'user@gmail.com',
+            'password' => bcrypt('123456')
+        ]);
+
+        $this->assertThrows(function() {
+            $this->authService->login('user@gmail.com', '1234567');
+        }, UnhauthorizationException::class);
+    }
+
+    public function test_login_should_throw_unhauthorization_exception_when_email_does_not_exists(): void
+    {
+        User::factory()->create([
+            'email' => 'user@gmail.com',
+            'password' => bcrypt('123456')
+        ]);
+
+        $this->assertThrows(function() {
+            $this->authService->login('ana@gmail.com', '123456');
+        }, UnhauthorizationException::class);
     }
 }
